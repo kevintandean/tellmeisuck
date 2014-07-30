e4f21089ea745119a0efb5061a3376a3016ac120
kevin <kevintandean@gmail.com>
2014-07-30 10:54:34 -0700

    2nd commit from pycharm

--- main/models.py
+++ main/models.py
@@ -12,7 +12,8 @@ class Post(models.Model):
         return self.author_name
 
 class UserProfile(models.Model):
-    name = models.CharField(max_length = 50)
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
     user_id = models.CharField(max_length = 20)
     created = models.DateTimeField(auto_now_add=True)
     new = models.CharField(max_length = 6)
