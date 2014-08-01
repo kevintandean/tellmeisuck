4f69691fedd25eaaf19603f657159294a13b16ed
Rudy Mutter <rudy@yetihq.com>
2014-08-01 15:06:40 -0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# Don't put spaces inbetween your '=' here
 class Post(models.Model):
     author = models.ForeignKey('UserProfile', related_name = 'post')
+    # Could come up with a better related_name then 'something'
     recipient = models.ForeignKey('UserProfile', related_name = 'something')
+    # You may also need to add blank=True here as well
     good = models.TextField(null=True)
     bad = models.TextField(null=True)
     created = models.DateTimeField(auto_now_add=True)
 
 
+# If you put UserProfile before Post you wouldn't need to put UserProfile in quotes in your Post model
+# You should be extended Django's AbstractUser model here and registering this in your settings.py file
+# instead of making your own with not all the appropriate fields
 class UserProfile(models.Model):
     first_name = models.CharField(max_length = 20)
     last_name = models.CharField(max_length = 20)
