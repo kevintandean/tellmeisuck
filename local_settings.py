9dc968321e76fe3575b232527324f7538b5a6fc3
kevin <kevintandean@gmail.com>
2014-08-04 17:59:25 -0700

    commit

--- local_settings.py
+++ local_settings.py
@@ -1,8 +1,8 @@
-__author__ = 'kevin'
-DATABASES = {'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'tellmeisuck',
-    'USER': 'postgres',
-    'PASSWORD': 'YYFhot2013'
-    }
-}
+# __author__ = 'kevin'
+# DATABASES = {'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': 'tellmeisuck',
+#     'USER': 'postgres',
+#     'PASSWORD': 'YYFhot2013'
+#     }
+# }
