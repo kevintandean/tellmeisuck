6abf1f2cb6bca4e6d824cb159db6962ab1921a4a
kevin <kevintandean@gmail.com>
2014-08-16 19:37:50 -0700

    general code update, bug fixes

--- tellmeisuck/settings.py
+++ tellmeisuck/settings.py
@@ -71,14 +71,18 @@ WSGI_APPLICATION = 'tellmeisuck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+
+##For Heroku deployment only
+
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
